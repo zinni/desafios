0184ca4bd85d4539875fc7fb3649ab7fb3ae9f0e
zinni <maxzinni@gmail.com>
2014-09-24 10:22:13 -0300

    Matriz e testes prontos, aleluia :)

--- matriz.py
+++ matriz.py
@@ -1,4 +1,10 @@
-matriz = [[0 for col in range(10)] for row in range(10)]
+class Matriz(object):
 
-for i in range(10):
-    matriz[i][i]=1 
+    def matrix(self):
+        matriz = [[0 for col in range(10)] for row in range(10)]
+        for i in range(10):
+            matriz[i][i]=1
+        return matriz
+
+if __name__ == '__main__':
+    pass
--- test_matriz.py
+++ test_matriz.py
@@ -1,14 +1,29 @@
 from unittest import TestCase
+from matriz import Matriz
 
 class TestMatriz(TestCase):
 
 
     def test_matriz_0x0(self):
-        esperado = 0
-        resposta = matriz[0][0]
+        matriz = Matriz()
+        esperado = 1
+        resposta = matriz.matrix()[0][0] 
         self.assertEqual(resposta, esperado)
 
     def test_matriz_5x5(self):
+        matriz = Matriz()
+        esperado = 1
+        resposta = matriz.matrix()[5][5]
+        self.assertEqual(resposta, esperado)
+
+    def test_matriz_1x2(self):
+        matriz = Matriz()
         esperado = 0
-        resposta = matriz[5][5]
+        resposta = matriz.matrix()[1][2]
+        self.assertEqual(resposta, esperado)
+
+    def test_matriz_9x8(self):
+        matriz = Matriz()
+        esperado = 0
+        resposta = matriz.matrix()[9][8]
         self.assertEqual(resposta, esperado)
